ed3ffbf805692d5913d875de3360c771e495855b
Ayden Diel <aydendiel@gmail.com>
2023-08-03 16:40:45 -0400

    Continued work on implementing Publish page

--- src/routes/Publish.tsx
+++ src/routes/Publish.tsx
@@ -1,17 +1,43 @@
 import { FormEvent } from "react";
 
-export default function Publish() {
-    function submitPost(e: FormEvent<HTMLFormElement>) {
-        e.preventDefault();
-        console.log("Publishing...");
+async function postJSON(data: Object) {
+    try {
+        const response = await fetch("/api/publish", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+
+        const result = await response.json();
+        console.log("Success:", result);
+    } catch (error) {
+        console.error("Error:", error);
     }
+}
+
+async function submitPost(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log("Publishing...");
+    const formData = new FormData(e.currentTarget);
+    const bodyData = formData.get("body");
+    const response: any = await postJSON({ body: bodyData, userID: 1 });
+    if (response instanceof Response) {
+        const result = await response.json();
+        console.log(result);
+    }
+}
+
+export default function Publish() {
+    
     return (
         <div>
             <form onSubmit={submitPost}>
-                <input type="text" placeholder="Username" />
-                <br/>
-                <input type="text" placeholder="body..." />
-                <br/>
+                <input name="username" type="text" placeholder="Username" />
+                <br />
+                <input name="body" type="text" placeholder="body..." />
+                <br />
                 <input type="submit" value="Publish" />
             </form>
         </div>
