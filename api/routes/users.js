243c1ebd18dd1d9f40f61273df342eba76839f8e
leiDnedyA <aydendiel@gmail.com>
2023-08-07 12:30:26 -0400

    squash me

---
+++ api/routes/users.js
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import User from '../models/User.js';
+
+const router = new Router();
+
+router.use('/api/user', async (req, res) => {
+    if (!req.query.id) {
+        res.status(400).send('Bad request, missing query param "id"')
+        return;
+    }
+
+    const user = await User.findOne({ where: { id: req.query.id } });
+
+    if (user) {
+        res.send(user);
+    } else {
+        res.status(404).send('User does not exist');
+    }
+
+});
+
+export default router;
